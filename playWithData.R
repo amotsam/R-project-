33eb372af1adbbfa1f24b3633e440a6e7a52edff
amotsam <amots2345@gmail.com>
2019-05-07 09:42:17 +0300

    Update playWithData.R

--- playWithData.R
+++ playWithData.R
@@ -1,5 +1,3 @@
-#set environment
-setwd("C:/Users/amotsam")
 
 #Packges needed 
 
@@ -16,14 +14,14 @@ shapesDF<-read.csv("shapes.csv")
       any(is.na(shapesDF))  #no missing values
       any(shapesDF == 0)    # no zeros
      
-       #question 1 boxplot
+       #boxplot
          
         boxplot(area~shape,data=shapesDF, main="area by shape", 
         xlab="shape", ylab="area distribution", 
         varwidth = FALSE)  #The maximum point of circles make it hard to read the plot
         
     
-       #question 2 max, mean and sd of area size per color
+       # max, mean and sd of area size per color
         summary(shapesDF$color) # blue, green, red, yellow
       
       
@@ -39,7 +37,7 @@ shapesDF<-read.csv("shapes.csv")
           max= max(area, na.rm=TRUE)
         )
       
-      #question 3- yeallow square average size is 3333.207
+      # yeallow square average size is 3333.207
       
         tb1<-subset(shapesDF ,color=="yellow" & shape== "square")
         mean(tb1$area)
@@ -53,7 +51,7 @@ shapesDF<-read.csv("shapes.csv")
           
         )
 
-      #question 4- most likely to be green is circle with 25.83%
+      # most likely to be green is circle with 25.83%
       tab<-table(shapesDF$shape, shapesDF$color)
       tab
       tab1prop<-prop.table(tab,margin=1)*100 #make table percentaged
@@ -61,13 +59,13 @@ shapesDF<-read.csv("shapes.csv")
       barplot(tab1prop,legend.text =T , col=c("red", "blue", "green"))
       
       
-      #question5.given object is red and areas size > 3000. whats the chances to any shape
+      #given object is red and areas size > 3000. whats the chances to any shape
       
       tab2<-subset(shapesDF,shapesDF$area>3000 & shapesDF$color=="red")
       tab2
       prop.table(summary(tab2$shape)) # answer 
       
-      # question 6 Function calc the side/radius of a shape
+      # Function calc the side/radius of a shape
       
          #Function gets 2 arguments: shapeN-String, area-Integer
          #return the side/radius depending on the shape
@@ -84,12 +82,12 @@ shapesDF<-read.csv("shapes.csv")
              return  (round(sqrt(area/(sqrt(3)/4))))
          }
            
-      # question7. apply the function 
+      # apply the function 
       
       table1<-mapply(shape_side,shapesDF$shape,shapesDF$area)
       shapesDF$side<-table1  
       
-      # question8 
+      # 
       
       #compare shape distribution by area and side 
       par(mfrow=c(1,2))
@@ -101,12 +99,7 @@ shapesDF<-read.csv("shapes.csv")
                xlab="shape", ylab="area distribution", 
                varwidth = TRUE)
        
-      # I can infer that distribution of side/radius by its shape, 
+      # We can infer that distribution of side/radius by its shape, 
       # is more symetric compare to area distribution by shape. 
-    
-      
-      
-      
-      
-                     
-      
+         
+                   
